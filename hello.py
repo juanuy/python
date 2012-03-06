00fbea358d642cb27fe7563c9e11286e24c4d799
juan <juan.uy@gmail.com>
2012-03-06 14:22:20 -0200

    second commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# hello.py: demo
+
+'''
+Este es un ejemplo
+'''
+
+print "Hola Mundo"
+
+nombre = raw_input("Ingresa tu nombre: ")
+print "Hello " + nombre + " too"
+  
+print "Saliendo..."
+
+
